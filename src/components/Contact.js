0ebe3c37603f7b51bf0b6b877246da29ad588ad5
GZMaster <33158650+GZMaster@users.noreply.github.com>
2023-10-19 18:51:18 +0100

    contact changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,97 +1,180 @@
-import React from 'react'
-import Skills from './Skills'
-import Phone from '../assets/img/call_icon.png'
-import Email from '../assets/img/email.png'
-import Location from '../assets/img/locate.png'
-import Facebook from '../assets/img/facebook.png'
-import Linkedin from '../assets/img/linkedin.png'
-import Twitter from '../assets/img/twitter.png'
-import Github from '../assets/img/github.png'
-import add_message from '../../serverfile/controller/messagecontroller'
+import React, { useState } from "react";
+import Skills from "./Skills";
+import Phone from "../assets/img/call_icon.png";
+import Email from "../assets/img/email.png";
+import Location from "../assets/img/locate.png";
+import Facebook from "../assets/img/facebook.png";
+import Linkedin from "../assets/img/linkedin.png";
+import Twitter from "../assets/img/twitter.png";
+import Github from "../assets/img/github.png";
 
 const Contact = () => {
-    return (
-        <>
-            <Skills></Skills>
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [message, setMessage] = useState("");
 
-            <section id='contact'>
+	const handleSubmit = async (e) => {
+		e.preventDefault();
 
-                <div className='ml-20 mr-20 mb-10'>
-                    <div className='flex'>
-                        <h3 className='font-secondary font-medium text-2xl mt-10'>Contact</h3>
-                        <hr className='mt-14 ml-5 border border-shadeGreen w-1/2' />
-                    </div>
+		const response = await fetch("/sendmessage", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				sendername: name,
+				email: email,
+				messagebody: message,
+			}),
+		});
 
-                    <div className='w-full flex ml-10 gap-20'>
-                        {/* Side bar */}
-                        <div className=' mt-10 shadow-lg rounded-md bg-green-100'>
-                            <div>
-                                <div className='p-5 flex gap-5'>
-                                    <img src={Phone} alt="Call icon" className='w-1/12' />
-                                    <h6>+2348182882010</h6>
-                                </div>
+		const resData = await response.json();
 
-                                <div className='p-5 flex gap-5'>
-                                    <img src={Email} alt="Message icon" className='w-1/12' />
-                                    <h6 className='pr-5'>isinwadeborah@gmail.com</h6>
-                                </div>
+		if (resData.status === "success") {
+			alert("Message sent.");
+		} else if (resData.status === "fail") {
+			alert("Message failed to send.");
+		}
+	};
 
-                                <div className='p-5 flex gap-5'>
-                                    <img src={Location} alt="Location icon" className='w-1/12 h-1/6' />
-                                    <h6 className='mt-2'>Benin-city, Nigeria</h6>
-                                </div>
+	return (
+		<>
+			<Skills></Skills>
 
-                                <div className='p-5 flex gap-5'>
-                                    <img src={Facebook} alt="Facebook icon" className='w-1/12' />
-                                    <a href="https://www.facebook.com/isinwaiheme.deborah" className='mt-1'>Deborah Isinwa-Iheme</a>
-                                </div>
+			<section id="contact">
+				<div className="ml-20 mr-20 mb-10">
+					<div className="flex">
+						<h3 className="font-secondary font-medium text-2xl mt-10">
+							Contact
+						</h3>
+						<hr className="mt-14 ml-5 border border-shadeGreen w-1/2" />
+					</div>
 
-                                <div className='p-5 flex gap-5'>
-                                    <img src={Linkedin} alt="Linkedin icon" className='w-1/12' />
-                                    <a href='https://www.linkedin.com/in/deborah-isinwa-iheme-ab037923a/' className='mt-1'>Benin-city, Nigeria</a>
-                                </div>
+					<div className="w-full flex ml-10 gap-20">
+						{/* Side bar */}
+						<div className=" mt-10 shadow-lg rounded-md bg-green-100">
+							<div>
+								<div className="p-5 flex gap-5">
+									<img src={Phone} alt="Call icon" className="w-1/12" />
+									<h6>+2348182882010</h6>
+								</div>
 
-                                <div className='p-5 flex gap-5'>
-                                    <img src={Twitter} alt="Twitter Icon" className='w-1/12' />
-                                    <a href="https://twitter.com/IsinwaDeborah?t=nQA686gt-P1iQ7mnLGp1Aw&s=08" className='mt-1'>@Isinwa-Deborah</a>
-                                </div>
+								<div className="p-5 flex gap-5">
+									<img src={Email} alt="Message icon" className="w-1/12" />
+									<h6 className="pr-5">isinwadeborah@gmail.com</h6>
+								</div>
 
-                                <div className='p-5 flex gap-5'>
-                                    <img src={Github} alt="Github icon" className='w-1/12' />
-                                    <a href='https://github.com/Isinwa-Deborah' className='mt-1'>Isinwa-Deborah</a>
-                                </div>
-                            </div>
-                        </div>
+								<div className="p-5 flex gap-5">
+									<img
+										src={Location}
+										alt="Location icon"
+										className="w-1/12 h-1/6"
+									/>
+									<h6 className="mt-2">Benin-city, Nigeria</h6>
+								</div>
 
+								<div className="p-5 flex gap-5">
+									<img src={Facebook} alt="Facebook icon" className="w-1/12" />
+									<a
+										href="https://www.facebook.com/isinwaiheme.deborah"
+										className="mt-1"
+									>
+										Deborah Isinwa-Iheme
+									</a>
+								</div>
 
-                        <div className='mt-10 bg-gray-100 p-10 rounded-md mr-20 shadow-lg'>
-                            <p>I am always open to <span className='font-bold'>discussing new projects, opportunities in tech world, partnerships</span>,
-                                and more so <span className='font-bold'>mentorship</span>. </p>
+								<div className="p-5 flex gap-5">
+									<img src={Linkedin} alt="Linkedin icon" className="w-1/12" />
+									<a
+										href="https://www.linkedin.com/in/deborah-isinwa-iheme-ab037923a/"
+										className="mt-1"
+									>
+										Benin-city, Nigeria
+									</a>
+								</div>
 
-                            <form action='/sendmessage' method='POST'>
-                                <div className='flex gap-9 mt-10'>
-                                    <label>Name:</label>
-                                    <input type="text" name="sendername" id="" className='border rounded-md border-green-400 py-2 px-6' />
-                                </div>
+								<div className="p-5 flex gap-5">
+									<img src={Twitter} alt="Twitter Icon" className="w-1/12" />
+									<a
+										href="https://twitter.com/IsinwaDeborah?t=nQA686gt-P1iQ7mnLGp1Aw&s=08"
+										className="mt-1"
+									>
+										@Isinwa-Deborah
+									</a>
+								</div>
 
-                                <div className='flex gap-10 mt-5'>
-                                    <label>Email:</label>
-                                    <input type="text" name="email" id="" className='border rounded-md border-green-400 px-6 py-2' />
-                                </div>
+								<div className="p-5 flex gap-5">
+									<img src={Github} alt="Github icon" className="w-1/12" />
+									<a href="https://github.com/Isinwa-Deborah" className="mt-1">
+										Isinwa-Deborah
+									</a>
+								</div>
+							</div>
+						</div>
 
-                                <div className='flex gap-4 mt-5'>
-                                    <label>Message:</label>
-                                    <textarea name="messagebody" id="" className='border rounded-md border-green-400 px-8 py-5 resize-none'></textarea>
-                                </div>
+						<div className="mt-10 bg-gray-100 p-10 rounded-md mr-20 shadow-lg">
+							<p>
+								I am always open to{" "}
+								<span className="font-bold">
+									discussing new projects, opportunities in tech world,
+									partnerships
+								</span>
+								, and more so <span className="font-bold">mentorship</span>.{" "}
+							</p>
 
-                                <button className='mt-10 font-tertiary font-semibold py-2 rounded-md px-5 text-lg bg-shadeGreen text-white hover:bg-gray-600' type='submit'>Submit</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </>
-    )
-}
+							<form
+								action="submit"
+								method="POST"
+								onSubmit={(e) => handleSubmit(e)}
+							>
+								<div className="flex gap-9 mt-10">
+									<label>Name:</label>
+									<input
+										type="text"
+										name="sendername"
+										id=""
+										value={name}
+										onChange={(e) => setName(e.target.value)}
+										className="border rounded-md border-green-400 py-2 px-6"
+									/>
+								</div>
 
-export default Contact
+								<div className="flex gap-10 mt-5">
+									<label>Email:</label>
+									<input
+										type="text"
+										name="email"
+										id=""
+										value={email}
+										onChange={(e) => setEmail(e.target.value)}
+										className="border rounded-md border-green-400 px-6 py-2"
+									/>
+								</div>
+
+								<div className="flex gap-4 mt-5">
+									<label>Message:</label>
+									<textarea
+										name="messagebody"
+										id=""
+										value={message}
+										onChange={(e) => setMessage(e.target.value)}
+										className="border rounded-md border-green-400 px-8 py-5 resize-none"
+									></textarea>
+								</div>
+
+								<button
+									className="mt-10 font-tertiary font-semibold py-2 rounded-md px-5 text-lg bg-shadeGreen text-white hover:bg-gray-600"
+									type="submit"
+								>
+									Submit
+								</button>
+							</form>
+						</div>
+					</div>
+				</div>
+			</section>
+		</>
+	);
+};
+
+export default Contact;
